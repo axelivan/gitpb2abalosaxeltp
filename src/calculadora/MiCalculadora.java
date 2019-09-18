ec9cc3e4d3275dfacb2197738a24d7fe4dc589fd
axeldos <axelabalosss@gmail.com>
2019-09-17 23:58:03 -0300

    se realizara un merge y se añadieron las funciones a la calculadora

--- src/calculadora/MiCalculadora.java
+++ src/calculadora/MiCalculadora.java
@@ -3,12 +3,45 @@ package calculadora;
 import java.util.Scanner;
 
 public class MiCalculadora {
+	 Scanner teclado = new Scanner(System.in);
+	 Integer a;
+	 Integer b;
+	 Integer resultado;
+	public void ingresar(){
+		
+		System.out.println("Ingrese el primer numero");
+ 		a = teclado.nextInt();
+ 		System.out.println("Ingrese el segundo numero");
+ 		b = teclado.nextInt();
+	}
+	public void sumar() {
+			resultado = a + b;
+ 	 		System.out.println("El resultado es " + resultado.toString());
+		
+	}
+	public void restar() {
+		resultado = a - b;
+	 		System.out.println("El resultado es " + resultado.toString());
+	}
+	
+	
+	public void multiplicar() {
+		 resultado = a * b;
+			System.out.println("El resultado es " + resultado.toString());
+	}
+	public void dividir() {
+		if (b!=0&&a!=0) {
+			resultado = a / b;
+ 			
+		System.out.println("El resultado es " + resultado.toString());
+		}else {System.out.println("esta cuenta no se puede hacer");}
+	}
+	
 	
  public static void main(String[] args) {
- Integer a;
- Integer b;
+
  Integer opcion;
- Integer resultado;
+ MiCalculadora calcu = new MiCalculadora();
  Scanner teclado = new Scanner(System.in);
  	do{
  		System.out.println("Ingrese su operación /n 1 para multiplicar /n 2 para restar /n 3 para sumar /n 4 para dividir");
@@ -16,30 +49,24 @@ public class MiCalculadora {
  	}
  	while (opcion < 1 && opcion > 4);
  		
- 		System.out.println("Ingrese el primer numero");
- 		a = teclado.nextInt();
- 		System.out.println("Ingrese el segundo numero");
- 		b = teclado.nextInt();
+ 		calcu.ingresar();
  		
  		
  		switch(opcion){
- 		case 1: resultado = a * b;
-		System.out.println("El resultado es " + resultado.toString());
+ 		case 1:
+ 			calcu.multiplicar();
  	
  			break;
  		case 2:
- 			resultado = a - b;
- 	 		System.out.println("El resultado es " + resultado.toString());
+ 			calcu.restar();
  			break;
  		case 3:		
- 			resultado = a + b;
- 	 		System.out.println("El resultado es " + resultado.toString());
+ 			calcu.sumar();
 	
  		break;
  		case 4:
- 			resultado = a / b;
- 			
- 		System.out.println("El resultado es " + resultado.toString());
+ 			calcu.dividir();
+
  		
  	}
  }}
